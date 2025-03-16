5446d4d2cebc031cfa064edd33c41979900d366a
Viktor <papenson622@gmail.com>
2025-03-16 09:36:58 +0300

    admin panel setup

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,12 @@
+import email
 from django.contrib import admin
+from carts.admin import CartTabAdmin
 from users.models import User
 # Register your models here.
 
 @admin.register(User)
 class CategoriesAdmin(admin.ModelAdmin):
-    list_display = ['username']
+    list_display = ['username', 'first_name', 'last_name', 'email']
+    search_fields = ['email', 'username', 'last_name']
+
+    inlines = [CartTabAdmin,]
